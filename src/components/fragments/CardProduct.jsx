01ea348220799849f545200be0540feacb8884ca
Rasyid Firmansyah <rasyidfirmansyah56@student.uns.ac.id>
2025-01-28 18:16:28 +0700

    🎨 Add rendering lists concept

--- src/components/fragments/CardProduct.jsx
+++ src/components/fragments/CardProduct.jsx
@@ -5,7 +5,7 @@ const CardProduct = (props) => {
   const { children } = props;
 
   return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2">
+    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
       {children}
     </div>
   );
@@ -22,13 +22,13 @@ const Header = (props) => {
 };
 
 const Body = (props) => {
-  const { title, children } = props;
+  const { name, children } = props;
 
   return (
-    <div className="px-5 pb-5">
+    <div className="px-5 pb-5 h-full">
       <a href="">
         <h5 className="text-xl font-semibold tracking-tight text-white">
-          {title}
+          {name}
         </h5>
         <p className="text-n text-white">{children}</p>
       </a>
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,28 +1,41 @@
 import CardProduct from "../components/fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "MSI Stealth 15",
+    price: 13999000,
+    desc: "Lorem ipsum dolor sit amet!",
+    image: "/src/assets/img/laptop.jpg",
+  },
+  {
+    id: 2,
+    name: "MSI Cyborg 15",
+    price: 12999000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quo dignissimos voluptatibus quisquam, maiores quam tempore porro aut eum, assumenda quas eaque! Doloremque cupiditate eaque molestiae porro! Non, culpa enim!",
+    image: "/src/assets/img/laptop.jpg",
+  },
+  {
+    id: 3,
+    name: "MSI Summit Evo 15",
+    price: 17999000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit!",
+    image: "/src/assets/img/laptop.jpg",
+  },
+];
+
 const ProductPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/src/assets/img/laptop.jpg" />
-        <CardProduct.Body title="MSI Stealth 15">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-          laborum sapiente numquam aspernatur reiciendis in distinctio excepturi
-          hic porro quas iusto officia eos, nam totam vel quia amet quod
-          molestias!
-        </CardProduct.Body>
-        <CardProduct.Footer price="RP 13.999.000" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/src/assets/img/laptop.jpg" />
-        <CardProduct.Body title="MSI Stealth 15">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-          laborum sapiente numquam aspernatur reiciendis in distinctio excepturi
-          hic porro quas iusto officia eos, nam totam vel quia amet quod
-          molestias!
-        </CardProduct.Body>
-        <CardProduct.Footer price="RP 13.999.000" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.desc}
+          </CardProduct.Body>
+          <CardProduct.Footer price={`Rp ${product.price}`} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
